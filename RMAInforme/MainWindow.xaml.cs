1117f2c4ba2f8cfe827dc83d200a5ee5488cb963
Jose Pompilio <milveintiuno@gmail.com>
2019-07-22 07:38:32 -0300

    BUBBA WORKS

--- RMAInforme/MainWindow.xaml.cs
+++ RMAInforme/MainWindow.xaml.cs
@@ -27,7 +27,7 @@ namespace RMAInforme
         private DateTime? periodoInicialSeleccionado;
         private DateTime? periodoFinalSeleccionado;
         private string sectorSeleccionado;
-        private string nombreServidor = "DESKTOP";
+        private string nombreServidor = "BUBBA";
         private int keywordINT;
         private string stringBusqueda;
         private string estadoSeleccionado;
