fa96331a2675665768ee6e30a16c7990ecd569ef
Jose Pompilio <milveintiuno@gmail.com>
2020-11-18 08:59:04 -0300

    agregado buscador de logs feo

--- RMAInforme/LogsWindow.xaml.cs
+++ RMAInforme/LogsWindow.xaml.cs
@@ -18,11 +18,10 @@ namespace RMAInforme
     {
         private string manualTestLog;
         private string burnintestLog;
-        private string supObsLog;
         private string obsLog;
         private int pixelCount = 0;
         private int reparadasCount = 0;
-        private string[] numeros;
+        private List<string> numeros;
         private string serverLogsRootPath = @"\\bubba\ea2100dc89ae9fe21fa9b08ab1bf18662dca1e53a3eebd7d03afebcaf5d57515$";
         private string localTempPath = Path.Combine(Path.GetTempPath(), "RMAInforme");
         private string localPedidoRootPath;
@@ -73,107 +72,131 @@ namespace RMAInforme
             return !Directory.EnumerateFileSystemEntries(path).Any();
         }
 
-        private void SearchLogs()
+        private async void SearchLogs()
         {
             using (new WaitCursor())
             {
                 if (string.IsNullOrWhiteSpace(this.numeroPedido)) { return; }
-
+                ResetVars();
+                tbKeyword.Text = tbKeyword.Text.ToUpper();
                 pedidoEnFormaDeRuta = Path.Combine(this.numeroPedido.Substring(0, 1), this.numeroPedido.Substring(1, 3), this.numeroPedido.Substring(4, 4));
                 serverPedidoRootPath = Path.Combine(serverLogsRootPath, pedidoEnFormaDeRuta);
 
                 if (Directory.Exists(serverPedidoRootPath) && !IsDirectoryEmpty(serverPedidoRootPath))
                 {
-                    localPedidoRootPath = Path.Combine(localTempPath, this.numeroPedido);
-                    CopyServerFilesToLocalTempDirectory();
-                    ProcessLocalFilesAndSetResults();
+                    await Task.Run(() =>
+                    {
+                        localPedidoRootPath = Path.Combine(localTempPath, this.numeroPedido);
+                        CopyServerFilesToLocalTempDirectory();
+                        ProcessLocalFilesAndSetResults();
+                    });
                 }
             }
         }
 
-        private async void CopyServerFilesToLocalTempDirectory()
+        private void ResetVars()
         {
-            await Task.Run(() =>
+            numeros = new List<string>();
+            lbNumbers.SelectedValue = null;
+            pixelCount = 0;
+            reparadasCount = 0;
+        }
+
+        private void CopyServerFilesToLocalTempDirectory()
+        {
+
+            string[] rutasServer = Directory.GetDirectories(serverPedidoRootPath, "*", SearchOption.TopDirectoryOnly);
+            //this.numeros = rutasServer.Select(Path.GetFileName).ToArray();
+
+            foreach (var rutaServer in rutasServer)
             {
-                string[] rutasServer = Directory.GetDirectories(serverPedidoRootPath, "*", SearchOption.TopDirectoryOnly);
-                this.numeros = rutasServer.Select(Path.GetFileName).ToArray();
-                if (this.numeros != null)
+                var numeroDeMaquina = Path.GetFileName(rutaServer);
+                if (numeroDeMaquina.Contains('R'))
+                {
+                    reparadasCount++;
+                }
+
+                var archivoLog = (this.numeroPedido + numeroDeMaquina.Substring(0, 5) + ".txt");
+                var supObsFileServer = Path.Combine(serverPedidoRootPath, "ObservacionesSupervisor.txt");
+                var rutaArchivoLogServer = Path.Combine(rutaServer, archivoLog);
+                var rutaArchivoBitServer = Path.Combine(rutaServer, "BIT.log");
+                var rutaArchivoObsServer = Path.Combine(rutaServer, "observacion.txt");
+                var rutaArchivoPixelServer = Path.Combine(rutaServer, "pixel.trash");
+
+                var rutaCarpetaEquipoLocal = Path.Combine(localPedidoRootPath, numeroDeMaquina);
+                var supObsLocal = Path.Combine(localPedidoRootPath, "ObservacionesSupervisor.txt");
+                var rutaArchivoLogLocal = Path.Combine(rutaCarpetaEquipoLocal, archivoLog);
+                var rutaArchivoBitLocal = Path.Combine(rutaCarpetaEquipoLocal, "BIT.log");
+                var rutaArchivoObsLocal = Path.Combine(rutaCarpetaEquipoLocal, "observacion.txt");
+                var rutaArchivoPixelLocal = Path.Combine(rutaCarpetaEquipoLocal, "pixeles.txt");
+
+
+                try
+                {
+                    //pixeles
+                    if (File.Exists(rutaArchivoPixelServer))
+                    {
+                        numeroDeMaquina += "-P";
+                        rutaCarpetaEquipoLocal += "-P";
+                        rutaArchivoPixelLocal = Path.Combine(rutaCarpetaEquipoLocal, "pixeles.txt");
+                        rutaArchivoLogLocal = Path.Combine(rutaCarpetaEquipoLocal, archivoLog);
+                        rutaArchivoBitLocal = Path.Combine(rutaCarpetaEquipoLocal, "BIT.log");
+                        rutaArchivoObsLocal = Path.Combine(rutaCarpetaEquipoLocal, "observacion.txt");
+                        rutaArchivoPixelLocal = Path.Combine(rutaCarpetaEquipoLocal, "pixeles.txt");
+                        Directory.CreateDirectory(rutaCarpetaEquipoLocal);
+                        File.Copy(rutaArchivoPixelServer, rutaArchivoPixelLocal, true);
+                        pixelCount++;
+                    }
+                    else
+                    {
+                        Directory.CreateDirectory(rutaCarpetaEquipoLocal);
+                    }
+
+                    //Obs de supervisor
+                    if (File.Exists(supObsFileServer))
+                    {
+                        File.Copy(supObsFileServer, supObsLocal, true);
+                    }
+
+                    //copio archivo log 'censurado'
+                    if (File.Exists(rutaArchivoLogServer))
+                    {
+                        var logContent = File.ReadAllText(rutaArchivoLogServer);
+                        var censoredLog = RemoveKey(logContent);
+                        File.WriteAllText(rutaArchivoLogLocal, censoredLog);
+                    }
+
+                    //log burnintest
+                    if (File.Exists(rutaArchivoBitServer))
+                    {
+                        File.Copy(rutaArchivoBitServer, rutaArchivoBitLocal, true);
+                    }
+
+                    //observaciones
+                    if (File.Exists(rutaArchivoObsServer))
+                    {
+                        File.Copy(rutaArchivoObsServer, rutaArchivoObsLocal, true);
+                    }
+
+                    this.numeros.Add(numeroDeMaquina);
+                }
+                catch (IOException e)
                 {
                     Application.Current.Dispatcher.Invoke(() =>
                     {
-                        lbNumbers.ItemsSource = numeros;
+                        MessageBox.Show(e.Message);
                     });
                 }
 
-                foreach (var rutaServer in rutasServer)
+            }
+
+            if (this.numeros != null)
+            {
+                Application.Current.Dispatcher.Invoke(() =>
                 {
-                    var numeroDeMaquina = Path.GetFileName(rutaServer);
-                    if (numeroDeMaquina.Contains('R'))
-                    {
-                        reparadasCount++;
-                    }
-                    var archivoLog = (this.numeroPedido + numeroDeMaquina.Substring(0, 5) + ".txt");
-
-                    var supObsFileServer = Path.Combine(serverLogsRootPath, "ObservacionesSupervisor.txt");
-                    var rutaArchivoLogServer = Path.Combine(rutaServer, archivoLog);
-                    var rutaArchivoBitServer = Path.Combine(rutaServer, "BIT.log");
-                    var rutaArchivoObsServer = Path.Combine(rutaServer, "observacion.txt");
-                    var rutaArchivoPixelServer = Path.Combine(rutaServer, "pixel.trash");
-
-                    var rutaCarpetaEquipoLocal = Path.Combine(localPedidoRootPath, numeroDeMaquina);
-                    var supObsLocal = Path.Combine(localPedidoRootPath, "ObservacionesSupervisor.txt");
-                    var rutaArchivoLogLocal = Path.Combine(rutaCarpetaEquipoLocal, archivoLog);
-                    var rutaArchivoBitLocal = Path.Combine(rutaCarpetaEquipoLocal, "BIT.log");
-                    var rutaArchivoObsLocal = Path.Combine(rutaCarpetaEquipoLocal, "observacion.txt");
-                    var rutaArchivoPixelLocal = Path.Combine(rutaCarpetaEquipoLocal, "pixeles.txt");
-
-                    try
-                    {
-                        Directory.CreateDirectory(rutaCarpetaEquipoLocal);
-
-
-                        if (File.Exists(supObsFileServer))
-                        {
-                            File.Copy(supObsFileServer, supObsLocal, true);
-                        }
-
-                        //copio archivo log 'censurado'
-                        if (File.Exists(rutaArchivoLogServer))
-                        {
-                            var logContent = File.ReadAllText(rutaArchivoLogServer);
-                            var censoredLog = RemoveKey(logContent);
-                            File.WriteAllText(rutaArchivoLogLocal, censoredLog);
-                        }
-
-                        //log burnintest
-                        if (File.Exists(rutaArchivoBitServer))
-                        {
-                            File.Copy(rutaArchivoBitServer, rutaArchivoBitLocal, true);
-                        }
-
-                        //observaciones
-                        if (File.Exists(rutaArchivoObsServer))
-                        {
-                            File.Copy(rutaArchivoObsServer, rutaArchivoObsLocal, true);
-                        }
-
-                        //pixeles
-                        if (File.Exists(rutaArchivoPixelServer))
-                        {
-                            File.Copy(rutaArchivoPixelServer, rutaArchivoPixelLocal, true);
-                            pixelCount++;
-                        }
-                    }
-                    catch (IOException e)
-                    {
-                        Application.Current.Dispatcher.Invoke(() =>
-                        {
-                            MessageBox.Show(e.Message);
-                        });
-                    }
-                }
-
-            });
+                    lbNumbers.ItemsSource = numeros;
+                });
+            }
         }
 
         private string RemoveKey(string text)
@@ -183,30 +206,33 @@ namespace RMAInforme
             return censoredText;
         }
 
+        //luego de cargar un pedido
         private void ProcessLocalFilesAndSetResults()
         {
-            tbLog.Text = "SELECCIONE UN NUMERO PARA VER EL REGISTRO DE TEST MANUAL.";
+            var supObsLocal = Path.Combine(localPedidoRootPath, "ObservacionesSupervisor.txt");
 
-            tbBit.Text = "SELECCIONE UN NUMERO PARA VER EL REGISTRO DE BURNINTEST.";
-
-            if (File.Exists("archivo de obs de supervisor"))
+            if (File.Exists(supObsLocal))
             {
-                obsLog += File.ReadAllText(rutaArchivoSupObsLocal);
+                obsLog = File.ReadAllText(supObsLocal);
             }
             else
             {
-                obsLog = Environment.NewLine + "NO SE ENCONTRARON OBSERVACIONES DEL SUPERVISOR.";
+                obsLog = "No se encontraron observaciones del supervisor.";
             }
 
-            tbPx.Text = "CON PIXELES DAÑADOS: " + pixelCount.ToString();
-            tbRepa.Text = "REPARADAS: " + reparadasCount.ToString();
-            tbObsView.Text = obsLog;
+            Application.Current.Dispatcher.Invoke(() =>
+            {
+                tbLog.Text = "Seleccione un numero para ver el registro de test manual.";
+                tbBit.Text = "Seleccione un numero para ver el registro de burnintest.";
+                tbObsView.Text = obsLog;
+                tbPx.Text = "Equipos con pixeles dañados: " + pixelCount.ToString();
+                tbRepa.Text = "Reparaciones: " + reparadasCount.ToString();
+            });
         }
 
         //en el cambio de numero
         private void SetInfo(string numero)
         {
-
             var archivoLog = this.numeroPedido + numero.Substring(0, 5) + ".txt";
             var rutaCarpetaEquipoLocal = Path.Combine(localPedidoRootPath, numero);
             var rutaArchivoLogLocal = Path.Combine(rutaCarpetaEquipoLocal, archivoLog);
@@ -216,11 +242,11 @@ namespace RMAInforme
 
             if (File.Exists(rutaArchivoPixelLocal))
             {
-                obsLog = Environment.NewLine + "CANTIDAD DE PIXELES:" + File.ReadAllText(rutaArchivoPixelLocal) + Environment.NewLine;
+                obsLog = "Cantidad de pixeles dañados: " + File.ReadAllText(rutaArchivoPixelLocal) + Environment.NewLine;
             }
             else
             {
-                obsLog = Environment.NewLine + "NO SE ENCONTRÓ EL ARCHIVO DE PIXELES.";
+                obsLog = "Cantidad de pixeles dañados: 0" + Environment.NewLine;
             }
 
             if (File.Exists(rutaArchivoLogLocal))
@@ -229,7 +255,7 @@ namespace RMAInforme
             }
             else
             {
-                manualTestLog = "NO SE ENCONTRÓ UN LOG DE TEST MANUAL PARA ESTE NUMERO.";
+                manualTestLog = "No se encontró un log de test manual para este numero.";
             }
 
             if (File.Exists(rutaArchivoBitLocal))
@@ -238,7 +264,7 @@ namespace RMAInforme
             }
             else
             {
-                burnintestLog = "NO SE ENCONTRÓ UN LOG DE BURNINTEST PARA ESTE NUMERO.";
+                burnintestLog = "No se encontró un log de burnintest para este numero.";
             }
 
             if (File.Exists(rutaArchivoObsLocal))
@@ -247,20 +273,81 @@ namespace RMAInforme
             }
             else
             {
-                obsLog += "NO SE ENCONTRARON OBSERVACIONES PARA ESTE NUMERO.";
+                obsLog += "No se encontraron observaciones para este numero.";
             }
-
-            tbLog.Text = manualTestLog;
-            tbBit.Text = burnintestLog;
-            tbObsView.Text = obsLog;
-            tbPx.Text = "CON PIXELES DAÑADOS: " + pixelCount.ToString();
-            tbRepa.Text = "REPARADAS: " + reparadasCount.ToString();
-
+            Application.Current.Dispatcher.Invoke(() =>
+            {
+                tbLog.Text = manualTestLog;
+                tbBit.Text = burnintestLog;
+                tbObsView.Text = obsLog;
+                tbPx.Text = "Equipos con pixeles dañados: " + pixelCount.ToString();
+                tbRepa.Text = "Reparaciones: " + reparadasCount.ToString();
+            });
         }
 
         private void btnSaveBit_Click(object sender, RoutedEventArgs e)
         {
+            GuardarLogs();
+        }
 
+        private void GuardarLogs()
+        {
+            if (numeroPedido != null)
+            {
+                try
+                {
+                    if ((bool)rbSaveSelected.IsChecked)
+                    {
+                        if (lbNumbers.SelectedValue != null)
+                        {
+                            var folderName = GetSaveDir();
+                            if (folderName == "?") { return; }
+                            var selected = lbNumbers.SelectedValue.ToString();
+                            var saveDir = Path.Combine(folderName, "RMAInforme", numeroPedido + selected);
+                            var folderToCopy = Path.Combine(localPedidoRootPath, selected);
+                            Directory.CreateDirectory(saveDir);
+
+                            foreach (string file in Directory.GetFiles(folderToCopy, "*.*", SearchOption.TopDirectoryOnly))
+                                File.Copy(file, file.Replace(folderToCopy, saveDir), true);
+                            MessageBox.Show("Registros guardados correctamente.", "Guardar Logs", MessageBoxButton.OK, MessageBoxImage.Information);
+                        }
+                    }
+
+                    if ((bool)rbSaveAll.IsChecked)
+                    {
+                        var folderName = GetSaveDir();
+                        if (folderName == "?") { return; }
+                        var saveDir = Path.Combine(folderName, "RMAInforme", numeroPedido);
+                        foreach (string dirPath in Directory.GetDirectories(localPedidoRootPath, "*", SearchOption.AllDirectories))
+                            Directory.CreateDirectory(dirPath.Replace(localPedidoRootPath, saveDir));
+
+                        foreach (string newPath in Directory.GetFiles(localPedidoRootPath, "*.*", SearchOption.AllDirectories))
+                            File.Copy(newPath, newPath.Replace(localPedidoRootPath, saveDir), true);
+                        MessageBox.Show("Registros guardados correctamente.", "Guardar Logs", MessageBoxButton.OK, MessageBoxImage.Information);
+                    }
+
+
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Ocurrió un error guardando los registros en la ubicación seleccionada.", "Guardar Logs", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+        }
+
+        private string GetSaveDir()
+        {
+            System.Windows.Forms.DialogResult result;
+            string folderName = "?";
+            using (var dialog = new System.Windows.Forms.FolderBrowserDialog())
+            {
+                result = dialog.ShowDialog();
+                if (result.ToString() == "OK")
+                {
+                    folderName = dialog.SelectedPath;
+                }
+            }
+            return folderName;
         }
 
         private void lbNumbers_SelectionChanged(object sender, SelectionChangedEventArgs e)
